285a17892cb2604617bd3cf9b2ed523b74c68249
Lindsey Kuper <lindsey@composition.al>
2013-12-04 13:19:45 -0500

    Added some string parsing stuff to WYAS.

--- wyas.hs
+++ wyas.hs
@@ -2,6 +2,14 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+data LispVal = Atom String
+              | List [LispVal]
+              | DottedList [LispVal] LispVal
+              | Number Integer
+              | String String
+              | Bool Bool
+  deriving Show
+
 main :: IO ()
 main = do
   args <- getArgs
@@ -14,6 +22,16 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Right val -> "Found value: " ++ show val
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseExpr :: Parser LispVal
+parseExpr = parseString
