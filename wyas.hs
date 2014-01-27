50cc2a8731e21ab1ad1baab0d468ef12e32d8e15
nanosec <nanosec@users.noreply.github.com>
2014-01-27 16:30:59 +0900

    Fix equal to work properly on lists

--- wyas.hs
+++ wyas.hs
@@ -339,16 +339,19 @@ cdr [DottedList (_ : xs) x] = return $ DottedList xs x
 cdr [badArg] = throwError $ TypeMismatch "pair" badArg
 cdr badArgList = throwError $ NumArgs 1 badArgList
 
+eqvalList :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] -> ThrowsError LispVal
+eqvalList eqvalFunc [List arg1, List arg2] =
+    return $ Bool $ (length arg1 == length arg2) && (all eqvalPair $ zip arg1 arg2)
+    where eqvalPair (x1, x2) = case eqvalFunc [x1, x2] of
+                                 Right (Bool val) -> val
+                                 Left err -> False
+
 eqv :: [LispVal] -> ThrowsError LispVal
 eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
 eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
 eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
 eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
-eqv [(List arg1), (List arg2)] =
-    return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
-           where eqvPair (x1, x2) = case eqv [x1, x2] of
-                                      Right (Bool val) -> val
-                                      Left err -> False
+eqv arg@([List arg1, List arg2]) = eqvalList eqv arg
 eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
 eqv [_, _] = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
@@ -363,6 +366,8 @@ unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
                       return $ unpacked1 == unpacked2
 
 equal :: [LispVal] -> ThrowsError LispVal
+equal arg@([List arg1, List arg2]) = eqvalList equal arg
+equal [DottedList xs x, DottedList ys y] = equal [List $ xs ++ [x], List $ ys ++ [y]]
 equal [arg1, arg2] = do
   primitiveEquals <- liftM or $ mapM (unpackEquals arg2 arg2) anyUnpackers
   eqvEquals <- eqv [arg1, arg2]
