b643dcd3c5381d6680c39a803d8a2941e9f954b2
nanosec <nanosec@users.noreply.github.com>
2014-04-05 08:08:48 +0900

    Tweak cons, car, cdr

--- wyas.hs
+++ wyas.hs
@@ -447,23 +447,22 @@ unpackBool (Bool b) = return b
 unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
 
 cons :: [LispVal] -> ThrowsError LispVal
-cons [x, List []] = return $ List [x]
-cons [x, List xs] = return $ List $ x : xs
-cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
+cons [x, List xs] = return $ List (x:xs)
+cons [x, DottedList xs xlast] = return $ DottedList (x:xs) xlast
 cons [x1, x2] = return $ DottedList [x1] x2
 cons badArgList = throwError $ NumArgs 2 badArgList
 
 car :: [LispVal] -> ThrowsError LispVal
-car [List (x : xs)] = return x
-car [DottedList (x : xs) _] = return x
-car [badArg] = throwError $ TypeMismatch "pair" badArg
+car [List (x:_)] = return x
+car [DottedList (x:_) _] = return x
+car [badArg] = throwError $ TypeMismatch "non-empty list" badArg
 car badArgList = throwError $ NumArgs 1 badArgList
 
 cdr :: [LispVal] -> ThrowsError LispVal
-cdr [List (x : xs)] = return $ List xs
+cdr [List (_:xs)] = return $ List xs
 cdr [DottedList [_] x] = return x
-cdr [DottedList (_ : xs) x] = return $ DottedList xs x
-cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr [DottedList (_:xs) x] = return $ DottedList xs x
+cdr [badArg] = throwError $ TypeMismatch "non-empty list" badArg
 cdr badArgList = throwError $ NumArgs 1 badArgList
 
 eqvalList :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] ->
